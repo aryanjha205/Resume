0620971b98299b3ad8362ff1c6b7eee55ff6bae4
Aryan jha <152834383+aryanjha205@users.noreply.github.com>
2026-03-02 16:50:47 +0530

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'resume-ai-v2';
 const ASSETS_TO_CACHE = [
   '/',
-  '/static/manifest.json',
+  '/manifest.json',
   '/static/app_icon_512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
